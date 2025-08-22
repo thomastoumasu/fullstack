0d239176cf22087a506ff3effcd4e917138c2838
thomastoumasu <thomastoumasu@protonmail.com>
2025-08-22 18:16:17 +0200

    Delete Fullstack/blogId directory

--- Fullstack/blogId/index.js
+++
@@ -1,9 +0,0 @@
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const app = require('./app') //only imports the actual application from app.js, ...
-
-//... then starts the application.
-const PORT = config.PORT
-app.listen(PORT, () => {
-  logger.info(`--index.js: Server running on port ${PORT}`)
-})
--- Fullstack/blogId/readme.md
+++
@@ -1,7 +0,0 @@
-printf '\33c\e[3J';  npm run test
-
-npm run lint
-
-npm run dev (http://localhost:3003/api/blogs)
-
-backend written with async await or with callback functions (blogs__.js)
